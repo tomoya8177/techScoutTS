5e7cc9d02a68590d7004517fcb4a2026fff3e102
tomoya8177 <imai@narra.jp>
2022-11-28 19:03:12 +0900

    adding dist

--- src/App.svelte
+++ src/App.svelte
@@ -15,11 +15,26 @@
   import Departments from './Templates/Departments.svelte';
   import Department from './Templates/Department.svelte';
   import { initAgora } from './lib/chatSDK';
+  import AgoraRTC from 'agora-rtc-sdk-ng';
 
+  let channelParameters = {
+    // A variable to hold a local audio track.
+    localAudioTrack: null,
+    // A variable to hold a local video track.
+    localVideoTrack: null,
+    // A variable to hold a remote audio track.
+    remoteAudioTrack: null,
+    // A variable to hold a remote video track.
+    remoteVideoTrack: null,
+    // A variable to hold the remote user id.s
+    remoteUid: null,
+  };
   User.subscribe(async (user) => {
     if (user) {
     }
   });
+  var isSharingEnabled = false;
+  var isMuteVideo = false;
   onMount(async () => {
     const { data, error } = await supabase.auth.getUser();
     if (error) {
@@ -28,14 +43,69 @@
       return;
     }
     console.log(data.user);
-    if (!data.user.user_metadata?.cometChatUID) {
-      data.user.user_metadata.cometChatUID == user.uid;
-    }
+
     User.set({
       ...data.user,
       loggedIn: true,
     });
     let res = await initAgora(data.user.id);
+    document.getElementById('inItScreen').onclick = async function () {
+      if (isSharingEnabled == false) {
+        // Create a screen track for screen sharing.
+        channelParameters.screenTrack = await AgoraRTC.createScreenVideoTrack();
+        // Stop playing the local video track.
+        channelParameters.localVideoTrack.stop();
+        // Unpublish the local video track.
+        await agoraEngine.unpublish(channelParameters.localVideoTrack);
+        // Publish the screen track.
+        await agoraEngine.publish(channelParameters.screenTrack);
+        // Play the screen track on local container.
+        channelParameters.screenTrack.play(localPlayerContainer);
+        // Update the button text.
+        document.getElementById(`inItScreen`).innerHTML = 'Stop Sharing';
+        // Update the screen sharing state.
+        isSharingEnabled = true;
+      } else {
+        // Stop playing the screen track.
+        channelParameters.screenTrack.stop();
+        // Unpublish the screen track.
+        await agoraEngine.unpublish(channelParameters.screenTrack);
+        // Publish the local video track.
+        await agoraEngine.publish(channelParameters.localVideoTrack);
+        // Play the local video on the local container.
+        channelParameters.localVideoTrack.play(localPlayerContainer);
+        // Update the button text.
+        document.getElementById(`inItScreen`).innerHTML = 'Share Screen';
+        // Update the screen sharing state.
+        isSharingEnabled = false;
+      }
+    };
+    document.getElementById('localAudioVolume').addEventListener('change', function (evt) {
+      console.log('Volume of local audio :' + evt.target.value);
+      // Set the local audio volume.
+      channelParameters.localAudioTrack.setVolume(parseInt(evt.target.value));
+    });
+    // Set an event listener on the range slider.
+    document.getElementById('remoteAudioVolume').addEventListener('change', function (evt) {
+      console.log('Volume of remote audio :' + evt.target.value);
+      // Set the remote audio volume.
+      channelParameters.remoteAudioTrack.setVolume(parseInt(evt.target.value));
+    });
+    document.getElementById('muteVideo').onclick = async function () {
+      if (isMuteVideo == false) {
+        // Mute the local video.
+        channelParameters.localVideoTrack.setEnabled(false);
+        // Update the button text.
+        document.getElementById(`muteVideo`).innerHTML = 'Unmute Video';
+        isMuteVideo = true;
+      } else {
+        // Unmute the local video.
+        channelParameters.localVideoTrack.setEnabled(true);
+        // Update the button text.
+        document.getElementById(`muteVideo`).innerHTML = 'Mute Video';
+        isMuteVideo = false;
+      }
+    };
   });
 </script>
 
@@ -51,4 +121,10 @@
       <Route path="/create-card/:projectId" component={CreateCard} />
     {/if}
   </Router>
+  <button type="button" id="inItScreen">Share Screen</button>
+  <button type="button" id="muteVideo">Mute Video</button>
+  <br /><label> Local Audio Level :</label>
+  <input type="range" min="0" id="localAudioVolume" max="100" step="1" /><br />
+  <label> Remote Audio Level :</label>
+  <input type="range" min="0" id="remoteAudioVolume" max="100" step="1" />
 </main>
--- src/lib/chatSDK.js
+++ src/lib/chatSDK.js
@@ -12,7 +12,7 @@ let channelParameters = {
 let options = {
   appId: AGORA_APP_ID, // Pass your App ID here.
   channel: 'test', // Set the channel name.
-  token: '', // Pass your temp token here.
+  token: '007eJxTYBAsCDzv87qPpYRp4scdS51lvN6/OSnPuvqkiwNDyI0SlUkKDMaJlklJKcYWlibmxibJqalJqUbGiYlpxkmmiYmWycamJUUtyQ2BjAy/vr9lYIRCEJ+FoSS1uISBAQAvWiBS', // Pass your temp token here.
   uid: 0, // Set the user ID.
   ExpireTime: 60, // Set token expire time.
   serverUrl: AGORA_TOKEN_GENERATOR_SERVEWR_URL, // The base URL to your token server.
@@ -33,11 +33,108 @@ async function FetchToken(uid) {
   });
 }
 
+AgoraRTC.onMicrophoneChanged = async (changedDevice) => {
+  // When plugging in a device, switch to a device that is newly plugged in.
+  if (changedDevice.state === 'ACTIVE') {
+    localAudioTrack.setDevice(changedDevice.device.deviceId);
+    // Switch to an existing device when the current device is unplugged.
+  } else if (changedDevice.device.label === localAudioTrack.getTrackLabel()) {
+    const oldMicrophones = await AgoraRTC.getMicrophones();
+    oldMicrophones[0] && localAudioTrack.setDevice(oldMicrophones[0].deviceId);
+  }
+};
+
+AgoraRTC.onCameraChanged = async (changedDevice) => {
+  // When plugging in a device, switch to a device that is newly plugged in.
+  if (changedDevice.state === 'ACTIVE') {
+    localVideoTrack.setDevice(changedDevice.device.deviceId);
+    // Switch to an existing device when the current device is unplugged.
+  } else if (changedDevice.device.label === localVideoTrack.getTrackLabel()) {
+    const oldCameras = await AgoraRTC.getCameras();
+    oldCameras[0] && localVideoTrack.setDevice(oldCameras[0].deviceId);
+  }
+};
+
 //init
 const agoraEngine = AgoraRTC.createClient({ mode: 'rtc', codec: 'vp8' });
+// Dynamically create a container in the form of a DIV element to play the remote video track.
+const remotePlayerContainer = document.createElement('div');
+// Dynamically create a container in the form of a DIV element to play the local video track.
+const localPlayerContainer = document.createElement('div');
+// Specify the ID of the DIV container. You can use the uid of the local user.
+localPlayerContainer.id = options.uid;
+// Set the textContent property of the local video container to the local user id.
+localPlayerContainer.textContent = 'Local user ' + options.uid;
+// Set the local video container size.
+localPlayerContainer.style.width = '480px';
+localPlayerContainer.style.height = '480px';
+localPlayerContainer.style.padding = '15px 5px 5px 5px';
+// Set the remote video container size.
+remotePlayerContainer.style.width = '480px';
+remotePlayerContainer.style.height = '480px';
+remotePlayerContainer.style.padding = '15px 5px 5px 5px';
+agoraEngine.on('user-unpublished', (user) => {
+  console.log(user.uid + 'has left the channel');
+});
+AgoraRTC.onAutoplayFailed = () => {
+  // Create button for the user interaction.
+  const btn = document.createElement('button');
+  // Set the button text.
+  btn.innerText = 'Click me to resume the audio/video playback';
+  // Remove the button when onClick event occurs.
+  btn.onClick = () => {
+    btn.remove();
+  };
+  // Append the button to the UI.
+  document.body.append(btn);
+};
+agoraEngine.on('user-published', async (user, mediaType) => {
+  // Subscribe to the remote user when the SDK triggers the "user-published" event.
+  await agoraEngine.subscribe(user, mediaType);
+  console.log('subscribe success');
+  // Subscribe and play the remote video in the container If the remote user publishes a video track.
+  if (mediaType == 'video') {
+    // Retrieve the remote video track.
+    channelParameters.remoteVideoTrack = user.videoTrack;
+    // Retrieve the remote audio track.
+    channelParameters.remoteAudioTrack = user.audioTrack;
+    // Save the remote user id for reuse.
+    channelParameters.remoteUid = user.uid.toString();
+    // Specify the ID of the DIV container. You can use the uid of the remote user.
+    remotePlayerContainer.id = user.uid.toString();
+    channelParameters.remoteUid = user.uid.toString();
+    remotePlayerContainer.textContent = 'Remote user ' + user.uid.toString();
+    // Append the remote container to the page body.
+    document.body.append(remotePlayerContainer);
+    // Play the remote video track.
+    channelParameters.remoteVideoTrack.play(remotePlayerContainer);
+  }
+  // Subscribe and play the remote audio track If the remote user publishes the audio track only.
+  if (mediaType == 'audio') {
+    // Get the RemoteAudioTrack object in the AgoraRTCRemoteUser object.
+    channelParameters.remoteAudioTrack = user.audioTrack;
+    // Play the remote audio track. No need to pass any DOM element.
+    channelParameters.remoteAudioTrack.play();
+  }
+  // Listen for the "user-unpublished" event.
+  agoraEngine.on('user-unpublished', (user) => {
+    console.log(user.uid + 'has left the channel');
+  });
+});
 export const initAgora = async (uid) => {
   //let token = await FetchToken(uid);
-  return await agoraEngine.join(options.appId, options.channel, options.token, options.uid);
+  await agoraEngine.join(options.appId, options.channel, options.token, uid);
+  // Create a local audio track from the audio sampled by a microphone.
+  channelParameters.localAudioTrack = await AgoraRTC.createMicrophoneAudioTrack();
+  // Create a local video track from the video captured by a camera.
+  channelParameters.localVideoTrack = await AgoraRTC.createCameraVideoTrack();
+  // Append the local video container to the page body.
+  document.body.append(localPlayerContainer);
+  // Publish the local audio and video tracks in the channel.
+  await agoraEngine.publish([channelParameters.localAudioTrack, channelParameters.localVideoTrack]);
+  // Play the local video track.
+  channelParameters.localVideoTrack.play(localPlayerContainer);
+  console.log('publish success!');
 };
 console.log(agoraEngine);
 /*
